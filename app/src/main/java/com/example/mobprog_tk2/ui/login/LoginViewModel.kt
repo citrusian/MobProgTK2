98361075d5eb75da102466e56d7e8f18fab38a45
citrusian <115960900+citrusian@users.noreply.github.com>
2023-09-11 18:50:57 +0700

    [change] Regex Validation

--- app/src/main/java/com/example/mobprog_tk2/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/mobprog_tk2/ui/login/LoginViewModel.kt
@@ -41,17 +41,38 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
     }
 
     // A placeholder username validation check
+//    private fun isUserNameValid(username: String): Boolean {
+//        val specialCharacters = arrayOf("@", "#", "$", "%", "^", "&")
+//        return if (specialCharacters.any { it in username }) {
+//            Patterns.EMAIL_ADDRESS.matcher(username).matches()
+//        } else {
+//            username.isNotBlank()
+//        }
+//    }
+
+
+    // Username Validation
+    // Regex Test
     private fun isUserNameValid(username: String): Boolean {
-        val specialCharacters = arrayOf("@", "#", "$", "%", "^", "&")
-        return if (specialCharacters.any { it in username }) {
+//        val specialCharacters = [^aA-zZ]
+//        val specialCharacters = intArrayOf("^", "a"-"z")
+        val specialCharacters = Regex("[^a-zA-Z]")
+        return if (specialCharacters.containsMatchIn(username)) {
             Patterns.EMAIL_ADDRESS.matcher(username).matches()
         } else {
             username.isNotBlank()
         }
     }
 
-    // A placeholder password validation check
+    // Password Validation
     private fun isPasswordValid(password: String): Boolean {
+        val specialCharacters = Regex("[^a-zA-Z]")
+//        return if (specialCharacters.containsMatchIn(password)) {
+//            Patterns.EMAIL_ADDRESS.matcher(password).matches()
+//        } else {
+//            return password.length > 3
+//        }
+        // Regex sometimes needed in password
         return password.length > 3
     }
 }
